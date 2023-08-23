57a37edfb34d3bf60b71bfe9a447848aa01a2b53
hasangawr <hasangawr@gmail.com>
2023-08-23 19:56:55 +0530

    Create drawing pad function

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const container = document.querySelector('.container');
 
-console.log(container);
+const createDrawingPad = function (size = 16, outerDiv = container, outerDivSize = 720) {
+
+    for (let index = 0; index < size; index++) {
+
+        const rowDiv = document.createElement('div');
+        container.appendChild(rowDiv);
+
+        rowDiv.style.setProperty('flex', '1 1 auto');
+        rowDiv.style.setProperty('display', 'flex');
+        rowDiv.style.setProperty('flex-direction', 'row');
+
+        for (let index = 0; index < size; index++) {
+
+            const colDiv = document.createElement('div');
+            rowDiv.appendChild(colDiv);
+
+            colDiv.style.setProperty('flex', '1 1 auto');
+            colDiv.style.setProperty('border', '1px');
+            colDiv.style.setProperty('border-style', 'solid');
+            colDiv.style.setProperty('border-color', '#E5E4E2');
+        }
+    }
+
+}
+
+const main = function () {
+
+    container.style.setProperty('display', 'flex');
+    container.style.setProperty('flex-direction', 'column');
+
+    createDrawingPad();
+
+}
+
+main();
